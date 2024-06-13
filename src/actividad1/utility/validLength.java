9c93b9053f608150836ea87fe92cde39f8499900
FranciscoABruno <usr3760@salesianos-lacuesta.net>
2024-06-13 22:37:52 +0100

    Feat(App1) Actividad uno con BufferedOutputStream FileOutputStream

---
+++ src/actividad1/utility/validLength.java
@@ -0,0 +1,10 @@
+package actividad1.utility;
+
+public class validLength {
+
+    public static void lengthChecker(String userOption) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'lengthChecker'");
+    }
+
+}
